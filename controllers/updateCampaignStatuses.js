fd0aa7dcd1c924816465174a1cf126b93a3594ef
ParakramV <bravocharlieawesome@gmail.com>
2025-11-15 09:50:13 +0800

    Test

--- controllers/updateCampaignStatuses.js
+++ controllers/updateCampaignStatuses.js
@@ -1,7 +1,8 @@
-import poolManager from './utils/poolManager.js';
+import { closeClientPool } from "../config/poolManager.js";
 
 const updateCampaignStatuses = async (clientPool, clientId) => {
-    let query = 'SELECT campaign_id, campaign_name FROM campaigns';
+    let query = 'SELECT  * FROM sm_inventory_campaign_management WHERE status = "active" OR status = "scheduled"';
+
     try {
         const [rows] = await clientPool.query(query);
         console.log(`Campaigns for client ${clientId}:`, rows);
@@ -10,6 +11,9 @@ const updateCampaignStatuses = async (clientPool, clientId) => {
     catch (error) {
         console.error(`Error fetching campaigns for client ${clientId}:`, error);
         throw error;
+    }
+    finally {
+        closeClientPool(clientId);
     }
 }
 
--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import insiightPool from './config/insiightCommonPool.js';
-
+import {getClientPool} from './config/poolManager.js';
+import updateCampaignStatuses from './controllers/updateCampaignStatuses.js';
 
 const getInsiightClientNames = async () => {
     let query = 'SELECT client_id, db_name, client_name FROM sm_clients_info';
@@ -14,14 +15,19 @@ const getInsiightClientNames = async () => {
     finally {
         await insiightPool.end();
     }
+    const pool = getClientPool(parent_org_id);
+
 };
-
-
 
 const main = async () => {
     try {
         const clients = await getInsiightClientNames();
-        console.log('Insiight Clients:', clients);
+        for (const client of clients) {
+            const clientPool = getClientPool(client.client_id);
+            const campaignRows = await updateCampaignStatuses(clientPool, client.client_id);
+            console.log(`Client: ${client.client_name}, Campaigns:`, campaignRows);
+        }
+        console.log('Insiight Clients:', );
     } catch (error) {
         console.error('Error in main execution:', error);
     }
